df743f2b15ed41758ee11b5977fffb59741cf31d
Anirban <anirbansharma36@gmail.com>
2023-01-18 21:20:16 +0530

    added file

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('hello'))
+
+app.listen(8080, () => {console.log('server started on 8080')})
